211c81f7488b86204adff2c399a3369b02e395d8
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:06:10 +0530

    Insert commands: Table Requirements
    Adding dummy data to table requirements.

---
+++ Data/ins_ranking.sql
@@ -0,0 +1,5 @@
+insert into ranking values('C00001', 120, 10, 14, NULL, NULL);
+insert into ranking values('C00002', NULL, 16, 23, 890, NULL);
+insert into ranking values('C00003', 17, 80, NULL, 78, 14);
+insert into ranking values('C00004', 18, NULL, NULL, NULL, NULL);
+insert into ranking values('C00005', NULL, 89, 14, 56, 780);
