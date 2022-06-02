5d821db0403290dcef20121d1a16330bde35ac81
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:07:00 +0530

    Insert commands: Table Stream
    Adding dummy data to table stream.

---
+++ Data/ins_stream.sql
@@ -0,0 +1,7 @@
+insert into stream values('ST001', 'Computer Science');
+insert into stream values('ST002', 'Data Science');
+insert into stream values('ST003', 'Electrical');
+insert into stream values('ST004', 'Mechanical');
+insert into stream values('ST005', 'Chemical');
+insert into stream values('ST006', 'Biotech');
+insert into stream values('ST007', 'Civil');
