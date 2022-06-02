e741051cc163658aeee9da6c2fd74ab64f6c3bc4
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:03:56 +0530

    Insert commands: Table Degree
    Adding dummy data to table degree.

---
+++ Data/ins_degree.sql
@@ -0,0 +1,8 @@
+insert into degree values('D00001', 'C00001', 'ST001', 'MTech', 'CS', 120);
+insert into degree values('D00002', 'C00001', 'ST002', 'MTech', 'DS', 120);
+insert into degree values('D00003', 'C00002', 'ST001', 'MTech', 'CS', 120);
+insert into degree values('D00004', 'C00002', 'ST001', 'MTech', 'Cybersec', 120);
+insert into degree values('D00005', 'C00003', 'ST005', 'MTech', 'Chemical', 120);
+insert into degree values('D00006', 'C00004', 'ST003', 'MTech', 'Electrical', 120);
+insert into degree values('D00007', 'C00005', 'ST001', 'MTech', 'CS', 120);
+insert into degree values('D00008', 'C00005', 'ST003', 'MTech', 'Electrical', 120);
