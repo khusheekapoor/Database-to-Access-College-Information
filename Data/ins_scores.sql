7418904312dfa8ec15326a0faa64d2604a340c87
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:05:31 +0530

    Insert commands: Table Scores
    Adding dummy data to table scores.

---
+++ Data/ins_scores.sql
@@ -0,0 +1,4 @@
+insert into scores values('S00001', 4, 9.9, NULL, 80, NULL, NULL, NULL, NULL);
+insert into scores values('S00002', 3, 9.1, 8, NULL, NULL, NULL, 450, NULL);
+insert into scores values('S00003', 4, 7.4, NULL, 70, NULL, 510, NULL, NULL);
+insert into scores values('S00004', 3, 8.9, 7, NULL, NULL, NULL, NULL, 150);
