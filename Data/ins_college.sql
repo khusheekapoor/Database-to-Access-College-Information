7ed32986c5be45cb5f06c48c70dbe49fa3636478
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:01:42 +0530

    Insert commands: Table College
    Adding dummy data to table college.

---
+++ Data/ins_college.sql
@@ -0,0 +1,5 @@
+insert into college(college_id, password, college_name, email, country) values('C00001', 'C1P', 'Waterloo', 'waterloo@w.com', 'Canada');
+insert into college(college_id, password, college_name, email, country) values('C00002', 'C2P', 'Toronto', 'toronto@t.com', 'Canada');
+insert into college(college_id, password, college_name, email, country) values('C00003', 'C3P', 'Arizona', 'arizona@a.com', 'USA');
+insert into college(college_id, password, college_name, email, country) values('C00004', 'C4P', 'Edinburgh', 'edinburgho@e.com', 'Scotland');
+insert into college(college_id, password, college_name, email, country) values('C00005', 'C5P', 'Speilburgh', 'speilburgh@s.com', 'Scotland');
