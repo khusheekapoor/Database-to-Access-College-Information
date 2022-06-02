ac382d88d350dbad6ade1672becd4486458023cf
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:02:10 +0530

    Insert commands: Table Aid
    Adding dummy data to table aid.

---
+++ Data/ins_aid.sql
@@ -0,0 +1,3 @@
+insert into aid values('C00001', 'Waterloo Scholarship', 'UoW', 'Canada');
+insert into aid values('C00001', 'Indian Scholarship', 'IaW', 'Canada');
+insert into aid values('C00003', 'USA Scholarship', 'UoAz', 'USA');
