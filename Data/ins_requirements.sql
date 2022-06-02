b6e394e82f948b1a1134101d17b4c5dfe352ef75
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-02 22:04:42 +0530

    Insert commands: Table Requirements
    Adding dummy data to table requirements.

---
+++ Data/ins_requirements.sql
@@ -0,0 +1,7 @@
+insert into requirements values('C00001', 'ST001', 'D00001', 4, '7.5',  NULL, 60, 170, NULL, NULL, NULL);
+insert into requirements values('C00001', 'ST002', 'D00002', 3, '8',  6, NULL, NULL, NULL, 400, NULL);
+insert into requirements values('C00002', 'ST001', 'D00003', 4, '7.5',  NULL, 60, NULL, 500, NULL, NULL);
+insert into requirements values('C00002', 'ST001', 'D00004', 3, '7.5',  6, NULL, NULL, NULL, NULL, 150);
+insert into requirements values('C00003', 'ST005', 'D00005', 3, '7.5',  NULL, 60, NULL, 500, NULL, NULL);
+insert into requirements values('C00004', 'ST003', 'D00006', 4, '7.5',  NULL, 60, 170, NULL, NULL, NULL);
+insert into requirements values('C00005', 'ST003', 'D00008', 4, '7.5',  NULL, 60, 170, NULL, NULL, NULL);
