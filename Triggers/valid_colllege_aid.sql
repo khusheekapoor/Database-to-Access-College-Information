9e837c06c9cd11d4f84c99821929516e60e68737
Khushee Kapoor <74901388+khusheekapoor@users.noreply.github.com>
2022-06-06 11:56:01 +0530

    Valid College Aid
    To be fired when an invalid College ID is entered when adding a new aid.

---
+++ Triggers/valid_colllege_aid.sql
@@ -0,0 +1,10 @@
+create or replace trigger college_not_found_aid before insert on aid for each row declare
+cursor c_col is select college_id from college; counter number := 0;
+begin
+for i in c_col loop
+if i.college_id=:new.college_id then counter := counter+1; end if;
+end loop;
+if counter=0 then
+raise_application_error(-20001, 'No such college exists'); end if;
+end;
+/
